9cc9cb6b0fff5776d38caa8fd1bb043bfb84bdd1
Justin-K-Ellis <jlke45@gmail.com>
2025-02-07 20:00:20 +0900

    Add custom CSS class for navbar

--- src/components/custom-components/Navbar.tsx
+++ src/components/custom-components/Navbar.tsx
@@ -1,18 +1,35 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
-    <nav className="flex justify-between items-center p-3 mb-4">
+    <nav className="flex justify-between items-center p-3 mb-4 border-b-2 border-black border-solid shadow">
       <div className="text-3xl font-bold">Next Gify</div>
       <div className="flex gap-2 text-xl">
-        <div>
-          <Link href="/">Home</Link>
+        <div className="nav-link">
+          <Link href="/" className={pathname === "/" ? "underline" : ""}>
+            Home
+          </Link>
         </div>
-        <div>
-          <Link href="/about">About</Link>
+        <div className="nav-link">
+          <Link
+            href="/about"
+            className={pathname === "/about" ? "underline" : ""}
+          >
+            About
+          </Link>
         </div>
-        <div>
-          <Link href="/contact">Contact</Link>
+        <div className="nav-link">
+          <Link
+            href="/contact"
+            className={pathname === "/contact" ? "underline" : ""}
+          >
+            Contact
+          </Link>
         </div>
       </div>
     </nav>
